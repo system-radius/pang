d0d32d87bab7f192bb9a281eb12ea099978dee76
Darius Orias <radius.sairo@gmail.com>
2022-03-28 00:32:47 +0800

    Add bg music, as well as ball pop, player killed, bullet fire sfx

---
+++ Assets/Scripts/ScriptableObjects/Sound.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+[CreateAssetMenu]
+public class Sound : ScriptableObject
+{
+    public AudioClip clip;
+    
+    [Range(0f, 1f)]
+    public float volume;
+
+    [Range(0.1f, 3f)]
+    public float pitch;
+}
---
+++ Assets/Scripts/UIScripts/GameSoundController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class GameSoundController : MonoBehaviour
+{
+    [SerializeField] private Sound playerHurt = null;
+
+    [SerializeField] private Sound ballPop = null;
+
+    AudioSource source;
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void PlayHurt()
+    {
+        source.clip = playerHurt.clip;
+        source.volume = playerHurt.volume;
+        source.pitch = playerHurt.pitch;
+        source.Play();
+    }
+
+    public void PlayPop()
+    {
+        source.clip = ballPop.clip;
+        source.volume = ballPop.volume;
+        source.pitch = ballPop.pitch;
+        source.Play();
+    }
+}
