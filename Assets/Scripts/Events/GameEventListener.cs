a79c2e164d92490f1bb3b370e080c125959aed05
Darius Orias <radius.sairo@gmail.com>
2022-03-27 19:09:08 +0800

    Add Main menu setup, transition to and from highscore view, exit game and start game events

---
+++ Assets/Scripts/Events/GameEventListener.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// A listener component that can be added to objects.
+/// Uses the game event listener concept from
+/// Unite 2017 - Game Architecture with Scriptable Objects.
+/// </summary>
+public class GameEventListener : MonoBehaviour
+{
+    public GameEvent gameEvent;
+    public UnityEvent response;
+
+    private void OnEnable()
+    {
+        if (gameEvent != null) gameEvent.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        if (gameEvent != null) gameEvent.UnregisterListener(this);
+    }
+
+    public void OnEventRaised()
+    {
+        response?.Invoke();
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/GameEvent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A scriptable object that will contain event registrations.
+/// Can create multiple instances of this object that may have varying events registered.
+/// Uses the game event concept from Unite 2017 - Game Architecture with Scriptable Objects.
+/// </summary>
+[CreateAssetMenu(fileName = "GameEvent", menuName = "Events/Game Event")]
+public class GameEvent : ScriptableObject
+{
+    private readonly List<GameEventListener> eventListeners =
+        new List<GameEventListener>();
+
+    public void Raise()
+    {
+        for (int i = eventListeners.Count - 1; i >= 0; i--)
+        {
+            eventListeners[i].OnEventRaised();
+        }
+    }
+
+    public void RegisterListener(GameEventListener listener)
+    {
+        if (!eventListeners.Contains(listener))
+        {
+            eventListeners.Add(listener);
+        }
+    }
+
+    public void UnregisterListener(GameEventListener listener)
+    {
+        if (eventListeners.Contains(listener))
+        {
+            eventListeners.Remove(listener);
+        }
+    }
+}
