ee8a5bf3fc5863d9b532dd2eaddc49abaa52dd82
Darius Orias <radius.sairo@gmail.com>
2022-03-28 00:23:42 +0800

    Add touch-screen controls

---
+++ Assets/Scripts/UIScripts/MobilePlatformChecker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobilePlatformChecker : MonoBehaviour
+{
+    [SerializeField] private RectTransform mobileControls = null;
+
+    void Awake()
+    {
+        if (Application.isMobilePlatform)
+        {
+            mobileControls.gameObject.SetActive(true);
+        }
+    }
+}
