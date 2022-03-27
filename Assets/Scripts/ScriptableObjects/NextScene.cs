68a954486f6e6395b2215f3d7775238e9e1f6396
Darius Orias <radius.sairo@gmail.com>
2022-03-27 19:13:35 +0800

    Add scene transition from main menu scene to the game scene

---
+++ Assets/Scripts/ScriptableObjects/NextScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Scene ID container so that they can be created
+/// and set to various objects.
+/// </summary>
+[CreateAssetMenu]
+public class NextScene : ScriptableObject
+{
+    public int sceneId = 0;
+}
---
+++ Assets/Scripts/UIScripts/NextSceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// A script component that will allow transition from
+/// one scene to another.
+/// </summary>
+public class NextSceneLoader : MonoBehaviour
+{
+    public NextScene scene;
+
+    public void LoadNextScene()
+    {
+        if (scene == null)
+        {
+            Debug.LogWarning("Null scene SO!");
+            return;
+        }
+        SceneManager.LoadScene(scene.sceneId);
+    }
+}
