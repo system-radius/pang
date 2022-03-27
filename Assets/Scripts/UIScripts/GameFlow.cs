192b4b8621b8c2bcac9f4144a89d434a19e5bf6b
Darius Orias <radius.sairo@gmail.com>
2022-03-27 20:01:51 +0800

    Add transition between subpanels for GameOver and LevelCleared

---
+++ Assets/Scripts/UIScripts/GameFlow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameFlow : MonoBehaviour
+{
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1;
+    }
+}
